2d20c2ba4be913cf8588d4cdb96fba0766eb4d0a
hauzemars <hzpart3@gmail.com>
2022-08-07 00:35:33 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          FINAL FUCKINLY LMAOOOOO
         </a>
       </header>
     </div>

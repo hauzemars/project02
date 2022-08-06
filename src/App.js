9bf0b77e61d6b9baea9a26c48f5a1a95b227d815
hauzemars <hzpart3@gmail.com>
2022-08-07 00:36:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          FINAL FUCKINLY LMAOOOOO
+          FINAL FUCKINLY LMAO
         </a>
       </header>
     </div>
